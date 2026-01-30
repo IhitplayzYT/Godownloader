2d659224161f94eb453802ae612ec2142cf80bc8
IhitplayzYT <ihitexe@gmail.com>
2026-01-30 12:37:32 +0530

    Added return code supposrt to use it in Goscrapper

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"os"
 )
 
-func main() {
+func run() int {
 	args := os.Args
 	l := len(args)
 	if l < 3 || l&1 == 0 {
 		usage(args[0])
-		return
+		return 0
 	}
 	url_path_map := parse_args(args[1:])
 	fmt.Println(url_path_map)
@@ -18,7 +18,12 @@ func main() {
 		err := download(url, path)
 		if err != nil {
 			fmt.Println("Error Occured", err)
+			return 0
 		}
 	}
+	return 1
+}
 
+func main() {
+	os.Exit(run())
 }
